8c19c7a291579cad70284e1243f15b9b2c7669b6
foyu <108912136+f0yu@users.noreply.github.com>
2023-10-31 00:03:12 +0800

    Update wifi_configuration.h

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -16,7 +16,7 @@ static struct {
     const char *password;
 } wifi_list[] __attribute__((unused)) = {
     {.ssid = "OTA", .password = "12345678"},
-    {.ssid = "DAP", .password = "12345678"},
+    {.ssid = "DAP1", .password = "12345678"},
     // Add your WAP like this:
     // {.ssid = "your ssid", .password = "your password"},
 };
